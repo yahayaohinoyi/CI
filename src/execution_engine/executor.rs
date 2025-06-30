f2559e1e2c481c010d9fea66d537395c55c487f2
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2025-06-30 22:11:51 +0100

    add some interfaces

--- src/execution_engine/executor.rs
+++ src/execution_engine/executor.rs
@@ -6,7 +6,15 @@ use bollard::models::ContainerCreateBody;
 use bollard::Docker;
 use uuid::Uuid;
 
-pub async fn execute(step: &Step) -> anyhow::Result<String> {
+pub async fn gen_container_logs(_: String) -> String {
+    todo!()
+}
+
+pub async fn start_container(_: String) -> anyhow::Result<()> {
+    todo!()
+}
+
+pub async fn create_container(step: &Step) -> anyhow::Result<String> {
     let docker = Docker::connect_with_local_defaults().context("Failed to connect to docker")?;
 
     let id = Uuid::new_v4();
